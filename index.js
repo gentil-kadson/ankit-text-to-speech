689e294de43a323dba5eb76f5fc552e4097556b2
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-06-20 10:16:19 -0300

    adds CORS and extension secrets.

--- index.js
+++ index.js
@@ -4,40 +4,57 @@ import dotenv from "dotenv";
 import { OpenAI } from "openai";
 
 dotenv.config();
+const CORS_CONFIG = {
+  origin: "chrome-extension://giahbgfobmennnohlplkigllpcflbick",
+  methods: ["POST"],
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(CORS_CONFIG));
 app.use(express.json());
 
 const openAi = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.post("/api/speech", async (req, res) => {
-  const { input } = req.body;
+app.post(
+  "/api/speech",
+  (req, res) => {
+    const authHeader = req.headers["x-extension-secret"];
 
-  try {
-    const audioData = await openAi.audio.speech.create({
-      model: "gpt-4o-mini-tts",
-      voice: "alloy",
-      input,
-    });
+    if (authHeader !== process.env.EXTENSION_SECRET)
+      return res
+        .status(403)
+        .json({ error: "You do not have permission to access this endpoint." });
 
-    const speechArrayBuffer = await audioData.arrayBuffer();
+    next();
+  },
+  async (req, res) => {
+    const { input } = req.body;
 
-    res.set({
-      "Content-Type": "audio/mpeg",
-      "Content-Length": speechArrayBuffer.byteLength,
-    });
+    try {
+      const audioData = await openAi.audio.speech.create({
+        model: "gpt-4o-mini-tts",
+        voice: "alloy",
+        input,
+      });
 
-    res.send(Buffer.from(speechArrayBuffer));
-  } catch (error) {
-    res.status(500).json({
-      error:
-        "Não foi possível gerar o áudio dos cartões. Tente novamente mais tarde",
-    });
+      const speechArrayBuffer = await audioData.arrayBuffer();
+
+      res.set({
+        "Content-Type": "audio/mpeg",
+        "Content-Length": speechArrayBuffer.byteLength,
+      });
+
+      res.send(Buffer.from(speechArrayBuffer));
+    } catch (error) {
+      res.status(500).json({
+        error:
+          "Não foi possível gerar o áudio dos cartões. Tente novamente mais tarde",
+      });
+    }
   }
-});
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
